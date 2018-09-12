93c325b49cea7b0553dd9319c3f6daa06d8f7909
Plopsis <kirvesmies70@hotmail.com>
2018-09-12 21:31:04 +0300

    derp

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _ready():
+	$center/vbox/start.connect("pressed",self,"_on_start_pressed")
+	$center/vbox/options.connect("pressed",self,"_on_options_pressed")
+	$center/vbox/quit.connect("pressed",self,"_on_quit_pressed")
 
 func _input(event):
 	if Input.is_action_just_pressed("shoot") && visible:
