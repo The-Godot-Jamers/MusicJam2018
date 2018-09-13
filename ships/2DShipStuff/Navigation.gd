f2fca98d39d37fcf17f3787877e7498869ce6a4d
Jeremi Biernacki <jeremi360@gmail.com>
2018-09-13 22:33:57 +0200

    rotate to mouse by the simplest way

--- ships/2DShipStuff/Navigation.gd
+++ ships/2DShipStuff/Navigation.gd
@@ -3,23 +3,21 @@ extends Navigation2D
 
 export var speed = 500.0
 export var keyboard_speed = 200.0
+export var rot_speed = 0.5
 
 var begin = Vector2()
 var end = Vector2()
 var path = []
-#var look_at = get_global_mouse_position()
 
 var process_path = true
-
 
 func _process(delta):
 	
 	handle_input(delta)
-	
 
 	if not process_path:
 		return
-
+	
 	if path.size() > 1:
 		var to_walk = delta * speed
 		while to_walk > 0 and path.size() >= 2:
@@ -34,28 +32,14 @@ func _process(delta):
 				to_walk = 0
 		
 		var atpos = path[path.size() - 1]
-		$Player.move_and_collide(atpos - $Player.position)
-		
+		var new_pos = atpos - $Player.position
+		$Player.move_and_collide(new_pos)
+
 		if path.size() < 2:
 			path = []
 			process_path = false
 	else:
 		process_path = false
-
-func rotate_const_speed(a, b, d): #In radians
-    if abs(a-b) >= PI: #only need to adjust for the long way round
-        if a > b:
-            b += 2 * PI
-        else:
-            a += 2 * PI
-    if a > b:
-        d = -d
-    return move_towards_float(a, b, d)
-
-func move_towards_float (x, target, step): #Moves towards t without passing it
-    if abs(target - x) < abs(step):
-        return target
-    else: return x + step
 
 func _update_path():
 	var p = get_simple_path(begin, end, true)
@@ -71,18 +55,15 @@ func nav_to(new_pos):
 
 
 func handle_input(delta):
-	
 	var new_pos = $Player.position
-	#var look_at = get_global_mouse_position()
-
-
+	var mouse_pos = get_global_mouse_position()
 
 	if Input.is_action_pressed("left_mouse_button"):
-		nav_to(get_global_mouse_position())
+		nav_to(mouse_pos)
+		$Player.look_at(mouse_pos)
 		return
 
 
-	
 	if Input.is_action_pressed("up"):
 		new_pos.y -= keyboard_speed * delta
 		$Player.rotation_degrees = 180
